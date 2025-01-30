c7e85dcb654f5fa113d127f03131ca4a9f669b36
FullIdle <2675423979@qq.com>
2025-01-30 15:47:10 +0800

    指令

--- src/main/kotlin/org/figsq/cobblemonpapi/cobblemonpapi/Main.kt
+++ src/main/kotlin/org/figsq/cobblemonpapi/cobblemonpapi/Main.kt
@@ -5,6 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin
 class Main:JavaPlugin(){
     override fun onEnable() {
         Papi.register()
+/*
         getCommand("cobblemonpapi")!!.setExecutor(this)
+*/
     }
 }
