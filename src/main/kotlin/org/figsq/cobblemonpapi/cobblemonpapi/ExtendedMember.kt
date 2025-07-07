f408eb53619b119e2caa28cdc877b4a684da0479
FullIdle <2675423979@qq.com>
2025-07-07 21:58:42 +0800

    优化biome处理

--- src/main/kotlin/org/figsq/cobblemonpapi/cobblemonpapi/ExtendedMember.kt
+++ src/main/kotlin/org/figsq/cobblemonpapi/cobblemonpapi/ExtendedMember.kt
@@ -1,6 +1,8 @@
 package org.figsq.cobblemonpapi.cobblemonpapi
 
 import com.cobblemon.mod.common.Cobblemon
+import com.cobblemon.mod.common.api.pokemon.PokemonProperties
+import com.cobblemon.mod.common.api.pokemon.PokemonSpecies
 import com.cobblemon.mod.common.api.spawning.CobblemonSpawnPools
 import com.cobblemon.mod.common.api.spawning.detail.PokemonSpawnDetail
 import com.cobblemon.mod.common.api.storage.PokemonStore
@@ -10,7 +12,9 @@ import com.cobblemon.mod.common.api.storage.party.PlayerPartyStore
 import com.cobblemon.mod.common.api.storage.pc.PCStore
 import com.cobblemon.mod.common.pokemon.Pokemon
 import com.cobblemon.mod.common.pokemon.Species
+import com.cobblemon.mod.common.util.asIdentifierDefaultingNamespace
 import com.google.gson.Gson
+import net.minecraft.ResourceLocationException
 import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
@@ -61,6 +65,45 @@ fun UUID.getOfflinePlayer(): OfflinePlayer? {
     return Bukkit.getOfflinePlayers().find { it.uniqueId == this };
 }
 
+val spawnDetailCache = mutableMapOf<Species, ()-> PokemonSpawnDetail?>()
+
+val Species.spawnDetail: PokemonSpawnDetail?
+    get() {
+        return spawnDetailCache.getOrPut(this) {
+            val detail = CobblemonSpawnPools.WORLD_SPAWN_POOL.find {
+                it is PokemonSpawnDetail && it.pokemon.matches(
+                    this,
+                    false
+                )
+            } as? PokemonSpawnDetail
+            {
+                detail
+            }
+        }()
+    }
+
 fun Pokemon.getSpawnDetail(): PokemonSpawnDetail? {
-    return CobblemonSpawnPools.WORLD_SPAWN_POOL.find { it is PokemonSpawnDetail && it.pokemon.matches(this) } as? PokemonSpawnDetail
+    return this.species.spawnDetail
+}
+
+/**
+ * 源码来自PokemonProperties内
+ * @see [PokemonProperties.commonMatches]
+ * @param def 当参数内没有species时候使用该默认值
+ * @return 是否匹配
+ */
+fun PokemonProperties.matches(pokemonSpecies: Species, def: Boolean): Boolean {
+    this.species?.run {
+        try {
+            val species = if (this == "random") {
+                PokemonSpecies.species.random()
+            } else {
+                PokemonSpecies.getByIdentifier(this.asIdentifierDefaultingNamespace()) ?: return@run
+            }
+            return pokemonSpecies == species
+        } catch (_: ResourceLocationException) {
+            return false
+        }
+    }
+    return def
 }
